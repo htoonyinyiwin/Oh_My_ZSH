6000ffc65feb0277dcdf855437a9b3d2e63797a6
Htoo Nyi Nyi Win <109453078+htoonyinyiwin@users.noreply.github.com>
2024-02-06 16:21:14 +0630

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+sudo apt update 
+sudo apt install zsh
+sudo apt-get install fonts-powerline
+
+chsh -s /usr/bin/zsh
+
+#install ohmyzsh using curl
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+
+#Or edit inside .zshrc file > ZSH_THEME="powerlevel10k/powerlevel10k"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+#Or edit inside .zshrc file > plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+source .zshrc
